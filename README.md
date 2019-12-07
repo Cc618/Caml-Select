751d8d985ff69bf6848affdf52bfe9156b9ab670
Celian RAIMBAULT <celian.raimbault@epita.fr>
2019-12-07 16:58:40 +0100

    Synced project

---
+++ README.md
@@ -0,0 +1,9 @@
+# Caml Select
+
+This extension aims to increase your Caml workflow.
+It provides two commands to select or run a Caml function.
+
+## Features
+
+* Select Caml function
+* Run Caml function
---
+++ extension.js
@@ -0,0 +1,75 @@
+// The module 'vscode' contains the VS Code extensibility API
+// Import the module and reference it with the alias vscode in your code below
+const vscode = require('vscode');
+
+// this method is called when your extension is activated
+// your extension is activated the very first time the command is executed
+
+// Selects the Caml Function
+// Returns whether the function is selected
+function selectFunc () {
+	let editor = vscode.window.activeTextEditor;
+	let data = editor.document.getText();
+	
+	// Find the start and end of the function
+	let cursorPos = editor.document.offsetAt(editor.selection.anchor);
+	let start = data.lastIndexOf('\nlet', cursorPos) + 1;
+	let end = data.indexOf(';;', cursorPos);
+
+	// If function found, update selection
+	if (end != -1)
+		editor.selection = new vscode.Selection(editor.document.positionAt(start), editor.document.positionAt(end + 2));
+	
+	return end != -1;
+}
+
+function getFunc() {
+	let editor = vscode.window.activeTextEditor;
+	let doc = editor.document;
+	let data = doc.getText();
+	
+	// Find the start and end of the function
+	let cursorPos = editor.document.offsetAt(editor.selection.anchor);
+	let start = data.lastIndexOf('\nlet', cursorPos) + 1;
+	let end = data.indexOf(';;', cursorPos);
+
+	// Not found
+	if (end == -1)
+		return '';
+
+	// Get command data (in selection)
+	return data.substring(start, end);
+}
+
+/**
+ * @param {vscode.ExtensionContext} context
+ */
+function activate(context) {
+	// Command to select the function
+	context.subscriptions.push(vscode.commands.registerCommand('extension.selectCamlFunction', selectFunc));
+
+	// Command to execute the function in the shell
+	context.subscriptions.push(vscode.commands.registerCommand('extension.runCamlFunction', function () {
+		// Open a terminal and run ocaml if there isn't one
+		if (vscode.window.terminals.length == 0) {
+			vscode.window.showErrorMessage('Please start a new terminal');
+			return;
+		}
+
+		// Retrieve the function
+		let cmd = getFunc();
+		
+		// Execute
+		if (cmd.length != 0)
+			vscode.window.activeTerminal.sendText(cmd);
+	}));
+}
+exports.activate = activate;
+
+// this method is called when your extension is deactivated
+function deactivate() {}
+
+module.exports = {
+	activate,
+	deactivate
+}
